b3fb05bc107c79df638b4aa5eafe77e72e82b584
sayo mizoguchi <sl.deve080412@gmail.com>
2021-10-06 18:26:13 -0700

    added react

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 9000;
+const path = require("path");
 
 //this is global middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/api/*", (_, res) => {
   res.json({
     data: "This is the API data",
   });
+});
+
+app.use("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "cliant/build", "index.html"));
 });
 
 app.listen(port, () => {
